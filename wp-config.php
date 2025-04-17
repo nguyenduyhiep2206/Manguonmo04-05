c8e77408630559b888c154699d9c14fdf78a087e
nguyenduyhiep2206 <nguyenduyhiep2206@gmail.com>
2025-04-17 19:27:38 +0700

    first commit

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,5 @@
 <?php
+define('WP_ALLOW_MULTISITE', true );
 /**
  * The base configuration for WordPress
  *
@@ -93,6 +94,12 @@ define( 'WP_DEBUG', false );
 define('FS_METHOD', 'direct');
 
 /* That's all, stop editing! Happy publishing. */
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', false );
+define( 'DOMAIN_CURRENT_SITE', 'localhost' );
+define( 'PATH_CURRENT_SITE', '/first-project/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
